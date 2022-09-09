5c77c24d450555eb30f25196604a16a078068f51
gianni <gianni.crivello96@gmail.com>
2022-09-09 13:11:37 -0700

    added installer

---
+++ installer.py
@@ -0,0 +1,16 @@
+#clone git repo into .overwatch
+#install node deps
+#install py deps
+import os
+import sys
+import subprocess
+if __name__ == "__main__":
+    path = os.getcwd()
+    subprocess.run(['git', 'clone', 'https://github.com/giannicrivello/overwatch.git'])
+    os.chdir('overwatch')
+    subprocess.run(['npm', 'i', 'package.json'])
+    os.chdir('../')
+    subprocess.run(['mkdir', '.overwatch'])
+    subprocess.run(['cp', '-rf', 'overwatch', '.overwatch'])
+    subprocess.run(['rm', '-rf', 'overwatch'])
+    sys.exit()
