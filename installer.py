3007572ef51d72f7399e1a72457deef5a3bdaff1
gianni <gianni.crivello96@gmail.com>
2022-09-09 13:18:15 -0700

    added installer

--- installer.py
+++ installer.py
@@ -7,7 +7,7 @@ import subprocess
 if __name__ == "__main__":
     path = os.getcwd()
     #subprocess.run(['git', 'clone', 'https://github.com/giannicrivello/overwatch.git'])
-    os.chdir('overwatch')
+    #os.chdir('overwatch')
     subprocess.run(['npm', 'i', 'package.json'])
     os.chdir('../')
     subprocess.run(['mkdir', '.overwatch'])
